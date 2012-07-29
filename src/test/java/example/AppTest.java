5c2e847ddaf2282693589bcb85517eacacc5812f
Sh Ueno <shueno.github@gmail.com>
2012-07-29 15:40:09 +0900

    Configure pom.xml for Eclipse

---
+++ src/test/java/example/AppTest.java
@@ -0,0 +1,13 @@
+package example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
